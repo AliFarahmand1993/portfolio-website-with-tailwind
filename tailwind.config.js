5a74974e7b874340141c4dc9dc1145f93c33f00f
Ali Farahmand <74143303+farahmandaliir@users.noreply.github.com>
2022-09-24 18:03:58 +0330

    create header
    create header
    add logo
    add nav
    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#A3A3FF',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
